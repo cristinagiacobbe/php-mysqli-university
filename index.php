7fcabd07ba3ed5ce6506ffb4ed7f3e3ee271c019
cristinagiacobbe <crigiak@gmail.com>
2024-04-05 23:36:10 +0200

    Query n.1

--- index.php
+++ index.php
@@ -6,20 +6,29 @@ define('DB_PASSWORD', 'root');
 define('DB_NAME', 'db_university');
 $connection = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-var_dump($connection);
+/* var_dump($connection); */
 
 if ($connection && $connection->connect_error) {
     echo 'Database connection failed';
     die;
 }
+/* $name = $_POST['name'];
+$sql = "SELECT * FROM `departments` WHERE `name` LIKE '%" . $name . "%' ";
+$result = $connection->query($sql); */
 
-$sql = "SELECT * FROM `departments`";
+/* var_dump($result); */
+
+/* if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        var_dump($row['name']);
+    }
+} */
+## TUTTI GLI STUDENTI NATI NEL 1990
+$sql = "SELECT * FROM `students` WHERE YEAR(`date_of_birth`) = 1990 ";
 $result = $connection->query($sql);
-var_dump($result);
 
-while ($row = $result->fetch_assoc()) {
-    /*    var_dump($row['name']); */
-}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -28,13 +37,27 @@ while ($row = $result->fetch_assoc()) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Db university</title>
+    <title>Students</title>
 </head>
 
 <body>
-    <?php while ($row = $result->fetch_assoc()) : ?>
-        <p><?= $row['name'] ?></p>
-    <?php endwhile; ?>
+
+    <a href="./departments.php">Departments</a>
+
+    <form action="" method="post">
+        <input type="text" name="name" id="name" placeholder="write">
+        <button type="submit">search</button>
+    </form>
+
+    <?php if ($result->num_rows > 0) : ?>
+        <?php while ($row = $result->fetch_assoc()) : ?>
+            <span> <?= $row['name'] ?></span>
+            <span> <?= $row['surname'] ?></span>
+            <span> <?= $row['date_of_birth'] ?></span>
+            <br>
+            <hr>
+    <?php endwhile;
+    endif; ?>
 </body>
 
 </html>
